7e02fb777371ea7bef2b96dd236edea7f4047dde
Filippo Benozzi <filippo.benozzi@dinamiza.it>
2020-02-11 10:52:55 +0100

    initial commit

---
+++ src/views/Hello/Hello.svelte
@@ -0,0 +1,7 @@
+<script>
+  export let name
+</script>
+
+<style src="./style.scss"></style>
+
+<h1>Hello, {name}!</h1>
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const PreloadWebpackPlugin = require('@vue/preload-webpack-plugin')
+const path = require('path')
+const { scss } = require('svelte-preprocess')
+
+const mode = process.env.NODE_ENV || 'development'
+const prod = mode === 'production'
+
+module.exports = {
+  entry: './src/main.js',
+  resolve: {
+    alias: {
+      '@views': path.resolve(__dirname, './src/views'),
+      '@assets': path.resolve(__dirname, './src/assets')
+    },
+    extensions: ['.mjs', '.js', '.svelte']
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'js/[name].[hash].js',
+    chunkFilename: 'js/[name].[hash].js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svelte$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'svelte-loader',
+          options: {
+            emitCss: true,
+            hotReload: true,
+            preprocess: require('svelte-preprocess')([scss()])
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          /**
+           * MiniCssExtractPlugin doesn't support HMR.
+           * For developing, use 'style-loader' instead.
+           * */
+          prod ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  mode,
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[hash].css'
+    }),
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+      minify: prod
+        ? {
+          collapseWhitespace: true
+        }
+        : false
+    }),
+    new PreloadWebpackPlugin({
+      rel: 'prefetch'
+    })
+  ],
+  devtool: prod ? false : 'source-map'
+}
